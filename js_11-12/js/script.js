fa6dee916b121168071d97a39a87017d9ad74e8c
Konstantin Nemchenko <nemchenko.k.k@gmail.com>
2016-04-22 17:43:18 +0300

    Fixed Task JS 11-12

--- js_11-12/js/script.js
+++ js_11-12/js/script.js
@@ -3,8 +3,10 @@ $(function() {
   $('.myCarousel').myCarousel({
     // positionPlugin : 'left'
   });
-  
+
 /////////////////// Работа с шаблонизатором ////////////////////////////////////
-  var tmpl = _.template(myTmpl);
+  var $template = $('#template').html();
+  console.log($template);
+  var tmpl = _.template($template);
   $('.templateTest').append(tmpl(user));
 });
