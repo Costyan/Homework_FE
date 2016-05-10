d55d7002e13ac90cc146e20658c0540eafdfc871
Konstantin Nemchenko <nemchenko.k.k@gmail.com>
2016-05-10 08:58:27 +0300

    Fixed Task JS 15-16

--- js_15-16/js/classes.js
+++ js_15-16/js/classes.js
@@ -26,3 +26,16 @@ var Student = {
     return 'Я сейчас смотрю сериал: ' + serials;
   },
 }
+
+Worker.name = 'Петя';
+Worker.age = 35;
+Student.name = 'Коля';
+Student.age = 19;
+
+console.dir(Human);
+console.log('Рабочего зовут: ', Worker.name);
+console.log('Ему ', Worker.age, ' лет');
+console.log('Рабочий отработал 20 дней и получил:', Worker.working(20));
+console.log('Студента зовут: ', Student.name);
+console.log('Ему ', Student.age, ' лет');
+console.log(Student.watchSerials('Сотня'));
