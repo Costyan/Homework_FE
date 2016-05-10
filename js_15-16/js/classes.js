e92bc4a213b05bcf2916a2f7de6a7018541674a8
Konstantin Nemchenko <nemchenko.k.k@gmail.com>
2016-05-10 12:26:14 +0300

    Fixed Task JS 15-16

--- js_15-16/js/classes.js
+++ js_15-16/js/classes.js
@@ -1,41 +1,63 @@
 'use strict';
-var Human = {
-  name  : 'Вася',
-  age  : 30,
-  sex   : 'мужчина',
-  height: '1.8 м.',
-  weight: '90 кг.'
+// var Human = {
+//   name  : 'Вася',
+//   age  : 30,
+//   sex   : 'мужчина',
+//   height: '1.8 м.',
+//   weight: '90 кг.'
+// };
+function Human(name, age, sex, height, weight)  {
+  this.name    = name || 'Вася';
+  this.age     = age || 30;
+  this.sex     = sex || 'муж.';
+  this.height  = height || '1.8 м.';
+  this.weight  = weight || '90 кг.';
+  this.units   = 'грн.';
 };
 
-var Worker = {
-  __proto__   : Human,
-  placeOfWork : 'Завод',
-  wages       : 3000,
-  units       : 'грн.',
-  working     : function(days) {
+function Worker(name, age, sex, height, weight, placeOfWork, wages) {
+  this.__proto__    = new Human();
+  this.name         = name || this.name;
+  this.age          = age || this.age;
+  this.sex          = sex || this.sex;
+  this.height       = height || this.height;
+  this.weight       = weight || this.weight;
+  this.placeOfWork  = placeOfWork || 'Завод';
+  this.wages        = wages || 3000;
+  this.working      = function(days) {
     return (this.wages / 30) * days + ' ' + this.units;
-  },
-}
+  };
+};
 
-var Student = {
-  __proto__     : Human,
-  placeOfStudy  : 'Университет',
-  stipend       : 1500,
-  units         : 'грн.',
-  watchSerials  : function(serials) {
-    return 'Я сейчас смотрю сериал: ' + serials;
-  },
-}
+function Student(name, age, sex, height, weight, placeOfStudy, stipend) {
+  this.__proto__    = new Human();
+  this.name         = name || this.name;
+  this.age          = age || this.age;
+  this.sex          = sex || this.sex;
+  this.height       = height || this.height;
+  this.weight       = weight || this.weight;
+  this.placeOfStudy = placeOfStudy || 'Университет';
+  this.stipend      = stipend || 1500;
+  this.watchSerials = function(serials) {
+    return serials;
+  };
+};
 
-Worker.name = 'Петя';
-Worker.age = 35;
-Student.name = 'Коля';
-Student.age = 19;
+var Vasya_Human = new Human();
+console.log(Vasya_Human);
+var Petya_Human = new Human('Петя', 35, 'муж.', '1.9 м.');
+console.log(Petya_Human);
 
-console.log(Human);
-console.log('Рабочего зовут:', Worker.name);
-console.log('Ему', Worker.age, 'лет');
-console.log('Рабочий отработал 20 дней и получил:', Worker.working(20));
-console.log('Студента зовут:', Student.name);
-console.log('Ему', Student.age, 'лет');
-console.log(Student.watchSerials('Сотня'));
+var Vasya_Worker = new Worker();
+console.log(Vasya_Worker);
+console.log(Vasya_Worker.name + ' отработал 10 дней и получил', Vasya_Worker.working(10));
+var Tolya_Worker = new Worker('Толя', 37, 'муж.', '1.7 м.');
+console.log(Tolya_Worker);
+console.log(Tolya_Worker.name + ' отработал 20 дней и получил', Tolya_Worker.working(20));
+
+var Vasya_Student = new Student();
+console.log(Vasya_Student);
+console.log('Студент ' + Vasya_Student.name + ' смотрит сериал:',Vasya_Student.watchSerials('Сотня'));
+var Olya_Student = new Student('Оля', 18, 'жен.', '1.5 м.', '45 кг.');
+console.log(Olya_Student);
+console.log('Студентка ' + Olya_Student.name + ' смотрит сериал:',Olya_Student.watchSerials('Рабыня Изаура'));
